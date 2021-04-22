c1575d75ced9cc7463757777c4679288060938de
Yehuda Hecht <yhecht1@gmail.com>
2021-04-22 16:34:11 -0400

    Convert to grayscale

--- Preprocessing/images_to_hist.py
+++ Preprocessing/images_to_hist.py
@@ -2,30 +2,12 @@ import os
 import preprocessing_functions
 import pandas as pd
 
-path = 'data/jpeg/train/'
+path = '../data/jpeg/train/'
 files = [image for image in os.listdir(path)]
 
 # Combine histogram and metadata
 hist_df = preprocessing_functions.files_to_hist_df(path, files)
-meta_df = pd.read_csv('data/train.csv')
-df = meta_df.merge(hist_df, on='image_name') # Necessary for correct indexing
-
-# Rename channels with color names
-numbers = df.loc[:,'0':'255'].columns
-rename_1 = dict(zip(df.loc[:,'256':'511'].columns, numbers))
-rename_2 = dict(zip(df.loc[:,'512':'767'].columns, numbers))
-
-# Blue
-blue = df.loc[:,'0':'255'].add_prefix('blue_')
-
-# Green
-df.loc[:,'256':'511'].rename(rename_1, axis=1, inplace=True)
-green = df.loc[:,'0':'255'].add_prefix('green_')
-
-# Red
-df.loc[:,'512':'767'].rename(rename_2, axis=1, inplace=True)
-red = df.loc[:,'0':'255'].add_prefix('red_')
-
-df = pd.concat([df.iloc[:,0:8],blue,green,red], axis=1)
+meta_df = pd.read_csv('../data/train.csv')
+df = meta_df.merge(hist_df, on='image_name')
 
 df.to_csv('../data/eda.csv')
--- Preprocessing/preprocessing_functions.py
+++ Preprocessing/preprocessing_functions.py
@@ -10,31 +10,24 @@ def files_to_hist_df(path, files_list):
     '''
     # Instantiate arrays
     files = [path+file for file in files_list]
-    blue_array = np.zeros(256)
-    green_array = np.zeros(256)
-    red_array = np.zeros(256)
+    hist_array = np.zeros(256)
     image_names = []
 
     for file in tqdm.tqdm(files_list):
         full_file = path+file
         image_names.append(file.split('.')[0])
-        img = cv2.imread(full_file, 3)
+        img = cv2.imread(full_file, 1)
         # Calculate histograms
-        blue = cv2.calcHist(img, channels=[0], mask=None, histSize=[256], ranges=[0,256]).flatten()
-        green = cv2.calcHist(img, channels=[1], mask=None, histSize=[256], ranges=[0,256]).flatten()
-        red = cv2.calcHist(img, channels=[2], mask=None, histSize=[256], ranges=[0,256]).flatten()
+        hist = cv2.calcHist(img, channels=[0], mask=None, histSize=[256], ranges=[0,256]).flatten()
+
         # Append histograms to arrays
-        blue_array = np.vstack([blue_array, blue])
-        green_array = np.vstack([green_array, green])
-        red_array = np.vstack([red_array, red])
+        hist_array = np.vstack([hist_array, hist])
 
     # Remove instantiating zeros
-    blue_array = blue_array[1:]
-    green_array = green_array[1:]
-    red_array = red_array[1:]
+    hist_array = hist_array[1:]
 
     # Create dataframes
-    hist_df = pd.DataFrame(np.hstack([blue_array, green_array, red_array]))
+    hist_df = pd.DataFrame(hist_array).add_prefix('intensity_')
     hist_df['image_name'] = image_names
 
     return hist_df
